86ffe8a1b2c7de1b83dfb16e58aaac75c9d713b1
Igor <101683017+IgorCastilhos@users.noreply.github.com>
2022-07-01 16:42:44 -0300

    Delete BeeCrowd1017.py

--- BeeCrowd1017.py
+++
@@ -1,8 +0,0 @@
-tempogasto = int(input())
-velocidademedia = int(input())
-distanciapercorrida = tempogasto * velocidademedia
-litrosnecessarios = distanciapercorrida / 12
-
-litrosnecessarios = float(litrosnecessarios)
-
-print("%.3f"%litrosnecessarios)
